32a1a3cfb77c6435cf282f026fa52cf396bf29c3
GitProdEnv <kopfhoererimtest@web.de>
2018-04-23 18:53:19 +0200

    publish

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
 import configureMockStore from 'redux-mock-store'; // this and following used for testing database state
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from "../../actions/expenses";
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+    const expensesData = {};
+    expenses.forEach(({ id, description, note, amount, createdAt }) => {
+        expensesData[id] = { description, note, amount, createdAt };
+    });
+    database.ref('expenses').set(expensesData).then(() => done());
+});
 
 test('should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc' });
@@ -75,7 +83,7 @@ test('should add expense to database and store', (done) => {  // WE have to do t
     });
 });
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
     const store = createMockStore({});
 
     // If no default on startAddExpense is given, we give back this const
@@ -99,6 +107,26 @@ test('should add expense with defaults to database and store', () => {
         return database.ref(`expenses/${actions[0].expense.id}`).once('value')
     }).then((snapshot) => {
         expect(snapshot.val()).toEqual(defaultExpense);
+        done();
+    });
+});
+
+test('should setup set expense action object with data', () => {
+    const action = setExpenses(expenses);
+    expect(action).toEqual({
+        type: 'SET_EXPENSES',
+        expenses
+    });
+});
+
+test('should fetch the expenses from firebase', (done) => { // provide done, letting Jest know not consider this test as succesful or failure after done is being called
+    const store = createMockStore({});
+    store.dispatch(startSetExpenses()).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+           type: 'SET_EXPENSES',
+           expenses // All of this data in the fixture file is exactly the data which we set in firebase
+        });
         done();
     });
 });
